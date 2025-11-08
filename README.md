bbd620b84b8a77760212914d005ea38e1f106a76
caelondev <jerichopanagsagan0@gmail.com>
2025-11-08 23:19:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TheophilusX's Webapp
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import config from "../config.json";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/invite", (req, res)=>{
+  res.redirect(config.botInviteLink)
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
